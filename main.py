eee47e87ce596c14be0570bcd2480cd8f6f8ee3b
FSRangelF <fs.rangel@gmail.com>
2025-11-10 22:35:43 -0300

    working game _ alfa version

--- main.py
+++ main.py
@@ -1,66 +1,97 @@
 from turtle import Turtle, Screen
 import random
+import time
 
-# 1. Create a Snake Body
-screensize = 800
-
+# setup screen
+screensize = 600
 screen = Screen()
 screen.setup(width=screensize, height=screensize)
 screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
 
-snake = Turtle(shape="square")
-snake.color("white")
-snake.speed("slowest")
-#snake.resizemode("user")
-#snake.shapesize(stretch_wid=1,stretch_len=4,outline=4)
+# setup snake
+snake_segments = []
+starting_position = [(0,0), (-20, 0), (-40, 0)]
 
-# 2.1 control the the snake
-def rotate_right():
-    snake.rt(90)
-
-def rotate_left():
-    snake.left(90)
-
-def increase_snake_size():
-    pass
-
-# 3. Create snake food
+# setup food
 food_x = 0
 food_y = 0
-def create_food():
-    food = Turtle(shape="circle")
-    food.color("blue")
-    food.resizemode("user")
-    food.shapesize(stretch_wid=1,stretch_len=1,outline=4)
-    global food_x 
-    food_x = random.randint(int(-screensize/2), int(screensize/2))
-    global food_y
-    food_y = random.randint(int(-screensize/2), int(screensize/2))
+food = Turtle(shape="circle")
+food.color("blue")
 
+def rotate_right():
+    snake_segments[0].right(90)
+
+def rotate_left():
+    snake_segments[0].left(90)
+
+def increase_snake_size():
+    segment = Turtle(shape="square")
+    segment.color("white")
+    segment.penup()
+    segment.setposition(snake_segments[-1].pos())
+    snake_segments.append(segment)
+
+def position_food():
+    global food_x 
+    global food_y
+    global food
+    food_x = 20*random.randint(int(-1*(screensize-20)/40), int((screensize-20)/40))
+    food_y = 20*random.randint(int(-1*(screensize-20)/40), int((screensize-20)/40))
     food.teleport(x=food_x, y=food_y)
 
-create_food()
+# create the snake
+for index in range(3):
+    segment = Turtle(shape="square")
+    segment.color("white")
+    segment.penup()
+    segment.setposition(starting_position[index])
+    snake_segments.append(segment)
 
-# 5. Create a scoreboard
+position_food()
+
+# Create a scoreboard
 score = 0
+print(score)
 
-# 2. Move the snake
+# move the snake
 game_over = False
 while not game_over:
-    snake.forward(10)
+    screen.update()
+    for index in range (len(snake_segments)-1, -1, -1):
+        if index == 0:
+            snake_segments[index].forward(20)
+        else:
+            new_position = snake_segments[index-1].position()
+            snake_segments[index].setposition(new_position)
+
+    # Control direction
     screen.listen()
-    screen.onkeypress(key="a", fun=rotate_right)
-    screen.onkeypress(key="d", fun=rotate_left)
-    # 4. Detect collision with food
-    if snake.xcor() == food_x and snake.ycor() == food_y:
+    screen.onkey(key="d", fun=rotate_right)
+    screen.onkey(key="a", fun=rotate_left)
+
+    # Detect collision with the food
+    if round(snake_segments[0].xcor(),0) == round(food_x, 0) and round(snake_segments[0].ycor(), 0) == round(food_y, 0):
         score += 1
+        position_food()
         increase_snake_size()
-        create_food()
+        print(score)
+   
+    # Detect collision with the wall
+    if round(snake_segments[0].xcor(),0) == -screensize/2 or round(snake_segments[0].xcor(),0) == screensize/2 or round(snake_segments[0].ycor(),0) == -screensize/2 or round(snake_segments[0].ycor(),0) == screensize/2:
+        game_over = True
 
+    # # detect collision with tail/body
+    for index in range(1, len(snake_segments)-1):
+        if round(snake_segments[0].xcor(),0) == round(snake_segments[index].xcor()) and round(snake_segments[0].ycor(), 0) == round(snake_segments[index].ycor()):
+            game_over = True
 
-
-
-# 6. Detect collision with the wall
-# 7. detect collision with tail/body
+    # increase speed as the score increases
+    if score < 10:
+        speed_factor = 1
+    else:
+        speed_factor = score // 5
+    time.sleep(0.2/speed_factor)
 
 screen.exitonclick()
